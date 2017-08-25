024a9f528f9a791eb4c5bbf349d5e69855106a2a
Florian Märkl <info@florianmaerkl.de>
2017-08-25 22:54:34 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
