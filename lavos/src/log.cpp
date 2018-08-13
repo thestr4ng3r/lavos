4ac9ee34d06d15f152272894c81e8bba0e860760
Florian Märkl <info@florianmaerkl.de>
2018-08-13 17:30:51 +0200

    Add Color to Logging

--- lavos/src/log.cpp
+++ lavos/src/log.cpp
@@ -10,19 +10,24 @@ using namespace lavos;
 void lavos::LogF(LogLevel level, const char *file, int line, const char *function, const char *fmt, ...)
 {
 	const char *level_str = nullptr;
+	const char *ansi_str = nullptr;
 	switch(level)
 	{
 		case LogLevel::Debug:
 			level_str = "D";
+			ansi_str = "\x1b[36m";
 			break;
 		case LogLevel::Info:
 			level_str = "I";
+			ansi_str = "";
 			break;
 		case LogLevel::Warning:
 			level_str = "W";
+			ansi_str = "\x1b[33m";
 			break;
 		case LogLevel::Error:
 			level_str = "E";
+			ansi_str = "\x1b[31m";
 			break;
 	}
 
@@ -32,10 +37,10 @@ void lavos::LogF(LogLevel level, const char *file, int line, const char *functio
 	else
 		filename++;
 
-	printf("[%s %16.16s:%-4d - %-20.20s] ", level_str, filename, line, function);
+	printf("%s[%s %16.16s:%-4d - %-20.20s] ", ansi_str, level_str, filename, line, function);
 	va_list ap;
 	va_start (ap, fmt);
 	vprintf(fmt, ap);
 	va_end (ap);
-	printf("\n");
+	printf("\x1b[0m\n");
 }
